319c08a5674f6b2d5eb4238c3e964dc7836083e6
jrgn9 <jorsko2@gmail.com>
2023-02-06 11:09:49 +0100

    move

--- oblig_nettverk/testforsok/test.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-HOST = '127.0.0.1'
-PORT = 9999
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((HOST, PORT))
-sock.send(b"GET / HTTP/1.1\r\nHost:HOST\r\n\r\n")
