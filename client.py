55747bb034c4714ddcaaeb3005b89a4b752afbd8
jrgn9 <jorsko2@gmail.com>
2023-02-28 13:50:49 +0100

    Laget feilmelding og bedre meldinger

--- client.py
+++ client.py
@@ -6,17 +6,17 @@ import socket
 import sys
 
 # Setter HOST, PORT og FILE til å være user argument 0,1 og 2
+# Jeg har valgt å ikke lage feilhåndtering for user input da det feilhåndteres på serversiden 
+# og hvis det ikke kommer en melding tilbake får brukeren en feilmelding uansett.
 HOST = sys.argv[1]
-PORT = int(sys.argv[2]) # Burde kanskje hatt error handling for PORT, men lot være i denne oppgaven.
+PORT = int(sys.argv[2])
 FILE = sys.argv[3]
 ADDR = (HOST, PORT)
 FORMAT = "utf-8"
-print(ADDR, FILE)
+print(f"[INPUT] {ADDR, FILE}")
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Definerer socket med socket familie og type
 
-
-#print("[ERROR] Could not connect")
 
 def recieve():
     try:
@@ -26,6 +26,6 @@ def recieve():
         sock.send(request)
         print(sock.recv(99999).decode(FORMAT))
     except:
-        pass
+        print("[ERROR] Could not connect, please try again")
 
 recieve()
